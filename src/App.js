e0cc73d6d80a9f6d0b88f677346ee86558ac56f0
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2022-10-13 00:11:31 +0500

    Added first expense component in our app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div>
       <h2>Hello world</h2>
       <h1>lets start our calculator</h1>
+      <ExpenseItem />
     </div>
   );
 }
